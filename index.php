2ba6eb36a6a57beeca66c999eba726f345df27e0
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-02-02 16:27:54 +0100

    calss Genre added

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
 
+class Genre{
+    public $genre;
+    
+    public function __construct($_genre){
+        $this->genre = $_genre;   
+    }
+}
+
 class Movie{
     public $titolo;
     public $dataUscita;
-    public $genre;
+    public $genres = [];
 
-    function __construct($_titolo, $_datauscita, $_genre){
+    function __construct($_titolo, $_datauscita, $_genres){
         
         $this->titolo = $_titolo;
         $this->dataUscita = $_datauscita;
-        $this->genre = $_genre;
+        $this->genres = $_genres;
     }
 
     public function getTitolo(){
@@ -18,8 +26,8 @@ class Movie{
 
 }
 
-$movie1 = new Movie("Guerre Stellari", 25-05-1977, "Fantascienza");
-$movie2 = new Movie("Rogue One", 15-12-2016 ,"Fantascienza");
+$movie1 = new Movie("Guerre Stellari", 25-05-1977, [new Genre("Fantascienza"), new Genre("Azione")]);
+$movie2 = new Movie("Rogue One", 15-12-2016 , [new Genre("Fantascienza"), new Genre("Azione")]);
 
 var_dump($movie1);
 echo  "<br>-------------------<br>";
